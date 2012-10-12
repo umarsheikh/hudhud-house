f92834dcbd085bbea5d4d70562135040a22cc832
Umar Sheikh <umar.m.sheikh@gmail.com>
2012-10-12 15:51:36 +0500

    without db

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HudhudHouse::Application.config.secret_token = '6438eddc0122a393236bbad5b9722796397e628bc25049c1ffcfcac151562201e4ec9d1e4d9de59c7aa1a1954095383e8e2d22d5df2294b52883d301f4bead5a'
+HudhudHouse::Application.config.secret_token = '1c6a136ef5cbb6f6c570588dcefdef1f8171a47ea6bd1811fdb2c2249a83b8655217464709d28ae0bf5c78fcc0b86653914019e3dc1a7232009018e8ace5d160'
