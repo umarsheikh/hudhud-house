8a07f61b691c7e906e76d6fd6698b871053d664e
Umar Sheikh <umar.m.sheikh@gmail.com>
2012-10-12 13:13:24 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HudhudHouse::Application.config.secret_token = '6438eddc0122a393236bbad5b9722796397e628bc25049c1ffcfcac151562201e4ec9d1e4d9de59c7aa1a1954095383e8e2d22d5df2294b52883d301f4bead5a'
